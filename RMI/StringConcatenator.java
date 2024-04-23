781b31583d5a88a906ff30db47a921ffe47bed0f
Anup Muttha <84242937+anupm652@users.noreply.github.com>
2024-04-23 21:50:20 +0530

    Delete RMI directory

--- RMI/StringConcatenator.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface StringConcatenator extends Remote {
-    String concatenateStrings(String s1, String s2) throws RemoteException;
-}
